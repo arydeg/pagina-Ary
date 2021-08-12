9027fa6985f6a944d0dc27f536c226748357d190
AryDegtiar <adegtiar@frba.utn.edu.ar>
2021-08-12 14:49:20 -0300

    MIGRACION A LA NUEVA PAGINA

--- javascript/micodigo.js
+++ javascript/micodigo.js
@@ -1,1 +1,9 @@
-﻿alert('hola mundoaaaaaaaa');
+﻿function mostrarCurriculum(){
+	
+	if(document.getElementById('curriculum').style.display == 'none'){
+		document.getElementById('curriculum').style.display = 'block';
+	}else{
+		document.getElementById('curriculum').style.display = 'none';
+	}
+		
+}
